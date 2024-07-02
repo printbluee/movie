13667b3508da6de3b4aafedf099ae4c71a964da8
with_blue <102470886+printbluee@users.noreply.github.com>
2024-07-02 15:38:32 +0900

    Update Config.js

--- client/src/components/Config.js
+++ client/src/components/Config.js
@@ -1,5 +1,5 @@
 
 export const API_URL = 'https://api.themoviedb.org/3/movie/';
-export const API_KEY = '0c7e40288576b9863285b914305a4528';
+export const API_KEY = '';
 export const IMAGE_BASE_URL = 'https://image.tmdb.org/t/p/';
 
